484ef4c9f1b4de3683176a96797c7825a3348efc
gbrink10 <62521661+gbrink10@users.noreply.github.com>
2025-05-21 20:48:53 -0500

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1747878533671

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_88e61792._.js",
+  "static/chunks/node_modules_93452dc0._.js"
+],
+    source: "dynamic"
+});
